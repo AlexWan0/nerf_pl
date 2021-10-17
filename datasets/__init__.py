10998167936f5280b644d63f166eccfd253ccac5
alexwan0 <wanalex03@gmail.com>
2021-10-17 15:35:28 -0700

    add tum

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,7 @@
 from .blender import BlenderDataset
 from .llff import LLFFDataset
+from .tum import TUMDataset
 
 dataset_dict = {'blender': BlenderDataset,
-                'llff': LLFFDataset}
+                'llff': LLFFDataset,
+                'tum': TUMDataset}
